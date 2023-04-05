5d46e5242ac612a31eb26f49432753b80018ab44
Jhalak Dashora <71014257+J2001d@users.noreply.github.com>
2023-04-05 18:57:37 +0530

    Create Exp - 15 Check a given number is prime or not.c

---
+++ Exp - 15 Check a given number is prime or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int checkPrime(int n) {
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	printf("%d", checkPrime(n));
+}
