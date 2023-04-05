fe4eba8cf5078f33d4ee10a9a29cf7e1813e4d3e
Jhalak Dashora <71014257+J2001d@users.noreply.github.com>
2023-04-05 18:45:13 +0530

    Create Exp - 11 Menu Driven Program.c

---
+++ Exp - 11 Menu Driven Program.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int menu() {
+	printf("\nPress 1 to add\n");
+	printf("Press 2 to subtract\n");
+	printf("Press 3 to exit\n");
+	int choice;
+	scanf("%d", &choice);
+	return choice;
+}
+int main() {
+	while (1) {
+		int choice = menu();
+		switch (choice) {
+		case 1: {
+			printf("Enter Two numbers to add\n");
+			int x, y;
+			scanf("%d %d", &x, &y);
+			printf("Addition is : %d\n", x + y);
+			break;
+		}
+		case 2: {
+			printf("Enter Two numbers to sub\n");
+			int x, y;
+			scanf("%d %d", &x, &y);
+			printf("Addition is : %d\n", x - y);
+			break;
+		}
+		case 3: {
+			exit(0);
+		}
+		}
+	}
+}
